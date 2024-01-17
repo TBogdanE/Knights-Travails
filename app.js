2858d0de5a157c4f6973859a8d165f8e8fd5a2b7
Toma Bogdan <tbogdane@TBE>
2024-01-17 16:22:26 +0200

    create 2d array and checks if input values aren't outside the border

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 class KnightsTravails {
   constructor() {
-    this.path = [];
+    this.path = {};
+    this.possibleMoves = [
+      [-2, -1],
+      [-2, 1],
+      [2, -1],
+      [2, 1],
+      [-1, -2],
+      [1, -2],
+      [-1, 2],
+      [1, 2],
+    ];
+  }
+
+  knightsMove(start, end) {
+    if (start[0] < 0 || start[0] > 7 || start[1] < 0 || start[1] > 7) {
+      console.error("Invalid starting position");
+      return;
+    }
+    if (end[0] < 0 || end[0] > 7 || end[1] < 0 || end[1] > 7) {
+      console.error("Invalid ending position");
+      return;
+    }
+    const graph = this.createGraph();
+
+    console.log(
+      `Start: ${start}; \n End: ${end};\n Graph: ${JSON.stringify(graph)}`
+    );
+  }
+
+  createGraph() {
+    let graph = [];
+    for (let i = 0; i < 8; i++) {
+      const row = [];
+      for (let j = 0; j < 8; j++) {
+        row.push([]);
+      }
+      graph.push(row);
+    }
+    console.log("gr", graph[1][2]);
+
+    return graph;
   }
 }
+
+const game = new KnightsTravails();
+game.knightsMove([3, 3], [4, 1]);

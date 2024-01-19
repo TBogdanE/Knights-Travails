9514c0ae18c357b5bdf33479bc970e0f141a8e08
Toma Bogdan <tbogdane@TBE>
2024-01-19 16:53:45 +0200

    user can now input values and it will console log the path

--- appUi.js
+++ appUi.js
@@ -1,23 +1,49 @@
+import { KnightsTravails } from "./app.js";
+
 const renderBoard = () => {
   let color = "black";
   const boardBox = document.getElementById("game-board");
   for (let i = 0; i < 8; i++) {
-    color = (color === "black") ? "white" : "black";
+    color = color === "black" ? "white" : "black";
     for (let j = 0; j < 8; j++) {
+      color = color === "black" ? "white" : "black";
       boardBox.appendChild(createBox(i, j, color));
-      color = (color === "black") ? "white" : "black";
     }
   }
 };
 
 const createBox = (posY, posX, color) => {
-  console.log(posY, posX);
   const box = document.createElement("div");
   box.id = `${posY}${posX}`;
-  console.log(box.id);
   box.classList.add("box");
   box.style.backgroundColor = color;
   return box;
 };
 
-renderBoard();
+const submitBtn = () => {
+  const submitBtn = document.getElementById("input-btn");
+
+  submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateMoves(getInput().startPos, getInput().endPos);
+  });
+};
+
+const getInput = () => {
+  const inputStart = document.getElementById("start-input").value;
+  const inputEnd = document.getElementById("end-input").value;
+  let startPos = inputStart.split(",").map(Number);
+  let endPos = inputEnd.split(",").map(Number);
+  return { startPos, endPos };
+};
+
+const updateMoves = (start, end) => {
+  let game = new KnightsTravails();
+  game.knightsMove(start, end);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderBoard();
+  submitBtn();
+  getInput();
+});
